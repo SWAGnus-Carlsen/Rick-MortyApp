a63079c15b5fbd07d909dbfbf95dc156c5976e5f
SWAGnus-Carlsen <92265364+SWAGnus-Carlsen@users.noreply.github.com>
2024-05-16 22:56:44 +0300

    project created

---
+++ RickAndMortyApp/RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Vitaliy Halai on 16.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
